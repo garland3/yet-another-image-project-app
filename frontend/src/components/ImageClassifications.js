b959a50b19ddf96225e88c6c58c3bc7017c9fe2a
Anthony Garland <garland3@gmail.com>
2025-08-30 16:45:16 +0000

    finally got th dev container to work

--- frontend/src/components/ImageClassifications.js
+++ frontend/src/components/ImageClassifications.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid'; // Add UUID library
 
 function ImageClassifications({ imageId, classes, loading, setLoading, setError }) {
   const [imageClassifications, setImageClassifications] = useState([]);
